3f102799b226caf7655885f7ede4fabf2d52bff8
Pavel Kalashnikov <pashka333d@gmail.com>
2023-11-25 21:30:12 +0300

    fix: ff

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const { PORT = 3000, DB_CONN } = process.env;
+const { PORT = 3000, DB_CONN = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose.connect(DB_CONN);
 
